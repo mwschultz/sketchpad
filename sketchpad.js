35f92143b01116aaaedcfb6b6fe4ef7e19430be4
Matt Schultz <matt@mwschultz.com>
2017-04-22 23:19:05 -0400

    Working copy!

--- sketchpad.js
+++ sketchpad.js
@@ -1,15 +1,44 @@
 $(document).ready(function() {
     console.log( "ready!" );
 
+
     for (i=0; i<16; i++) {
-      $(".container").append('<div class="row"></div>');
-    }
-    for (i=0; i<16; i++) {
-          $(".row").append('<div class="square"></div>');
+      $(".grid").append('<tr class="row"></tr>');
     }
 
-    $(".square").mouseover(function () {
-      $(this).addClass("color"); 
+    for (i=0; i<16; i++) {
+      $(".row").append('<td class="cell"></td>');
+    }
+
+
+    $( "#clear-grid" ).click(function() {
+    $(".cell").removeClass("color");
 
     });
+
+    $("#remake-grid").click(function() {
+      var x=prompt("How many squares?");
+      $(".grid").children('tr').remove();
+
+      for (i=0; i<x; i++) {
+        $(".grid").append('<tr class="row"></tr>');
+      }
+
+      for (i=0; i<x; i++) {
+        $(".row").append('<td class="cell"></td>');
+      }
+
+      $(".cell").mouseover(function () {
+        $(this).addClass("color");
+
+      });
+
+    });
+
+    $(".cell").mouseover(function () {
+      $(this).addClass("color");
+
+    });
+
+
 });
